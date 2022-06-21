0bee09be3d9d6d51cad84a47e1d039d6870e845b
Ankit Shaji <ankitshaji@gmail.com>
2022-06-21 14:46:33 +1200

    Reading $_GET and $_POST array values

--- index.php
+++ index.php
@@ -18,10 +18,47 @@
         <div class="buttons">
             <button type="submit">Submit</button>
             <button type="submit" formmethod="post">Submit with POST</button>
-            <button type="reset">Reset</button>
         </div>
 
     </form>
+    <br>
+    <?php
+    //print_r POST and GET arrays
+    print_r($_GET);
+    print_r($_POST);
+
+    //GET - parameters visible in url
+    //ALT
+    // $firstname = filter_input(INPUT_GET, "first", FILTER_SANITIZE_STRING);
+    // $lastname =  filter_input(INPUT_GET, "last", FILTER_SANITIZE_STRING);
+    if (isset($_GET["first"]) && isset($_GET["last"])) {
+        $firstname = $_GET["first"];
+        $lastname = $_GET["last"];
+        if (!empty($firstname) && !empty($lastname)) {
+            echo htmlspecialchars($firstname);
+            echo "<br>";
+            echo htmlspecialchars($lastname);
+        } else {
+            echo "<br>GET:empty error : Missing data";
+        }
+    } else {
+        echo "<br>GET:isset error: Not set";
+    }
+
+    //POST - parameters hidden in URL
+    if (isset($_POST["first"]) && isset($_POST["last"])) {
+        $firstname = $_POST["first"];
+        $lastname = $_POST["last"];
+        if (!empty($firstname) && !empty($lastname)) {
+            echo htmlspecialchars($firstname);
+            echo htmlspecialchars($lastname);
+        } else {
+            echo "<br>POST:empty error : Missing data";
+        }
+    } else {
+        echo "<br>POST:isset error: Not set";
+    }
+    ?>
 </body>
 
 </html>
